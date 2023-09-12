494458377ae00c8af078fc41568a46ba3a9c5528
SeAdil2023 <adilalx2023@gmail.com>
2023-09-12 08:05:30 -0700

    A

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * main - prints _putchar, followed by a new line,
+ * using _putchar prototype
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+	
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
