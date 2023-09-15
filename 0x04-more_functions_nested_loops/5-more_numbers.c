de0d70d8ab7ec0616b84ea8a5eaa277e81d41555
SeAdil2023 <adilalx2023@gmail.com>
2023-09-15 09:18:00 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@
 
 void more_numbers(void)
 {
-	int i, j, num;
+	int num, i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j < 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			num = j;
 			if (j > 9)
