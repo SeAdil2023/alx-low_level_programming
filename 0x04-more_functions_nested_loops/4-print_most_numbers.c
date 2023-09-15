2128b0d612dc23db76f235cfcf99b861b005d5b7
SeAdil2023 <adilalx2023@gmail.com>
2023-09-15 08:33:40 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,14 +11,13 @@
 
 void print_most_numbers(void)
 {
-	char digit;
+	int num;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (digit != '0' && digit != '4')
-		{
-			_putchar(digit);
-		}
+		if (num == 2 || num == 4)
+			continue;
+			_putchar(num + 48);
 	}
 	_putchar('\n');
 }
