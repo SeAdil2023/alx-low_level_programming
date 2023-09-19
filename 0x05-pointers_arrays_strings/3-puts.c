a10dc71daf80a1fa8d83e700fac72c9f5802a803
SeAdil2023 <adilalx2023@gmail.com>
2023-09-19 07:09:32 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string,
+ * followed by a new line, to stdout.
+ *
+ * @str: string parameter to print
+ *
+ * Returrn: Nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
