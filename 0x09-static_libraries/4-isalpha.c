482d39919f492697e9e808ad0f8d03e9dab2771b
SeAdil2023 <adilalx2023@gmail.com>
2023-10-02 08:43:54 -0700

    libmy

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character.
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
