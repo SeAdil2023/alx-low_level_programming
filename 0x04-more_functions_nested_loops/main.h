f3ac4c871e5d09755d27e66ce9b059e8e1c8e2a2
SeAdil2023 <adilalx2023@gmail.com>
2024-03-21 09:27:40 -0700

    updated

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include<stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 int _isupper(int c);
@@ -11,5 +9,9 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
